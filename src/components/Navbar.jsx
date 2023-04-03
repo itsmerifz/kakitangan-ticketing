a08d34b57932991029c6824c1338e21876682ebb
itsmerifz <azkklk@gmail.com>
2023-04-04 07:01:08 +0800

    add event card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
 const Navbar = () => {
+  const [navbarBg, setNavbarBg] = React.useState(true);
+  
+  const ChangeBackground = () => {
+    if (window.scrollY > 0) {
+      setNavbarBg(true);
+    } else {
+      setNavbarBg(false);
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", ChangeBackground);
+  });
+
   return (
-    <nav className="navbar bg-base-100">
+    <nav className={`navbar z-40 transition-colors duration-500 sticky ${ navbarBg ? 'bg-base-100' : 'bg-transparent' } top-0`}>
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -25,29 +39,43 @@ const Navbar = () => {
             tabIndex={0}
             className="flex flex-col gap-5 dropdown-content mt-3 p-2 shadow bg-base-100 w-52 border-2 border-black"
           >
-            <a className="hover:border-2 border-black" href="#">Beranda</a>
-            <a className="hover:border-2 border-black" href="#">Tiket</a>
-            <a className="hover:border-2 border-black" href="#">Login</a>
+            <a className="hover:border-2 border-black" href="#">
+              Beranda
+            </a>
+            <a className="hover:border-2 border-black" href="#">
+              Tiket
+            </a>
+            <a className="hover:border-2 border-black" href="#">
+              Login
+            </a>
           </ul>
         </div>
-        <h3 className="p-2 m-0 z-40">KKTNGN</h3>
+        <h3 className="p-2 m-0">KKTNGN</h3>
       </div>
       <div className="navbar-end hidden lg:flex">
         <ul className="flex gap-5 px-2">
-          <a className="border-2 border-transparent hover:border-black p-1" href="#">Beranda</a>
-          <a className="border-2 border-transparent hover:border-black p-1" href="#">Tiket</a>
-          <a className="border-2 border-transparent hover:border-black p-1" href="#">Login</a>
+          <a
+            className="border-2 border-transparent hover:border-black p-1"
+            href="#"
+          >
+            Beranda
+          </a>
+          <a
+            className="border-2 border-transparent hover:border-black p-1"
+            href="#"
+          >
+            Tiket
+          </a>
+          <a
+            className="border-2 border-transparent hover:border-black p-1"
+            href="#"
+          >
+            Login
+          </a>
         </ul>
       </div>
     </nav>
   );
 };
-
-const ChangeBackground = () => {
-  if(window.scrollY >= 77){
-    document.querySelector('.navbar').classList.add('bg-transparent');
-    document.querySelector('.navbar').classList.remove('bg-base-100');
-  }
-}
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GrMap } from "react-icons/gr";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       ) : (
         <CarouselPhone />
       )}
+      <ListEvent />
     </section>
   );
 };
@@ -31,14 +33,71 @@ const CarouselPhone = () => {
         </div>
       </div>
       <div className="flex items-center justify-center w-full rounded-none mt-[-65px] gap-4">
-        <a href="#item1" className="w-6 h-6 border-2 border-black bg-base-300 flex items-center justify-center">
+        <a
+          href="#item1"
+          className="w-6 h-6 border-2 border-black bg-base-300 flex items-center justify-center"
+        >
           1
         </a>
-        <a href="#item2" className="w-6 h-6 border-2 border-black bg-base-300 flex items-center justify-center">
+        <a
+          href="#item2"
+          className="w-6 h-6 border-2 border-black bg-base-300 flex items-center justify-center"
+        >
           2
         </a>
       </div>
     </>
+  );
+};
+
+const ListEvent = () => {
+  return (
+    <div className="p-3">
+      <div className="p-3 ms-2">
+        <h3 className="mb-3">Event</h3>
+        <p className="font-mono">Kumpulan event yang sedang berlangsung</p>
+      </div>
+      <div className="flex flex-col lg:flex-row gap-6 items-center justify-center lg:justify-start">
+        <div className="card card-compact w-72 shadow-xl z-0">
+          <figure className="m-0"><img src="/src/assets/img/bahas-bahasa.jpg" alt="Event Bahas Bahasa"  /></figure>
+          <div className="card-body">
+            <p className="card-title m-0 text-base">Bahas Bahasa</p>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-3 items-center">
+                <GrMap className="w-4 h-4" />
+                <p className="text-base font-mono m-0">Manado</p>
+              </div>
+              <div className="flex gap-3 items-center">
+                <GrMap className="w-4 h-4" />
+                <p className="text-base font-mono m-0">Manado</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <h3>Rp. 50.000</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card card-compact w-72 shadow-xl">
+          <figure className="m-0"><img src="/src/assets/img/bahas-bahasa.jpg" alt="Event Bahas Bahasa"  /></figure>
+          <div className="card-body">
+            <p className="card-title m-0 text-base">Bahas Bahasa</p>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-3 items-center">
+                <GrMap className="w-4 h-4" />
+                <p className="text-base font-mono m-0">Manado</p>
+              </div>
+              <div className="flex gap-3 items-center">
+                <GrMap className="w-4 h-4" />
+                <p className="text-base font-mono m-0">Manado</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <h3>Rp. 50.000</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 

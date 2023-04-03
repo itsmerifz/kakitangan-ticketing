88f59e7f07a7a2137e311f233505980ce695217c
itsmerifz <azkklk@gmail.com>
2023-04-03 22:45:34 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Layout from './components/Layouts'
+
+const App = () => {
+  return(
+    <Layout>
+      <h1>aijsaijsi</h1>
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/components/Layouts.jsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }) => {
+  return (
+    <div className="sm:max-w-xl w-full m-auto p-3 prose">
+      {children}
+    </div>
+  )
+}
+
+export default Layout
